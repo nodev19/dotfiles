103fba767138baff8f66a5b299d7f04a654a0b24
adam <nodev1@proton.me>
2024-03-03 12:40:03 +0000

    yada yada yada

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 -- shizzle
 vim.g.mapleader = " "
+vim.o.clipboard = "unnamedplus"
 vim.opt.mouse = ""
 vim.opt.list = true
 vim.opt.lcs = "tab:> ,lead:.,trail:."
@@ -27,6 +28,8 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 16
 vim.opt.signcolumn = "yes"
@@ -40,35 +43,42 @@ require("lazy").setup({
 		cmd = "Neoconf"
 	},
 	"folke/neodev.nvim",
+	"folke/trouble.nvim",
 	-- treesitter
 	"nvim-treesitter/nvim-treesitter",
 	-- telescope
 	"nvim-telescope/telescope.nvim",
 	"nvim-lua/plenary.nvim",
+	"LukasPietzschmann/telescope-tabs",
 	-- lsp zero
-	'VonHeikemen/lsp-zero.nvim',
-	'williamboman/mason.nvim',
-	'williamboman/mason-lspconfig.nvim',
-	'neovim/nvim-lspconfig',
-	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/nvim-cmp',
+	"VonHeikemen/lsp-zero.nvim",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/nvim-cmp",
 	'L3MON4D3/LuaSnip',
 	"VonHeikemen/lsp-zero.nvim",
 	-- debugger
 	"mfussenegger/nvim-dap",
 	"rcarriga/nvim-dap-ui",
-	"jay-babu/mason-nvim-dap.nvim",
 	"nvim-telescope/telescope-dap.nvim",
-	"theHamsta/nvim-dap-virtual-text",
 	-- git
 	"tpope/vim-fugitive",
 	"lewis6991/gitsigns.nvim",
 	-- other
 	"stevearc/aerial.nvim",
-	"stevearc/overseer.nvim",
+	"RaafatTurki/hex.nvim",
+	"nvim-tree/nvim-web-devicons",
+	{
+		"wakatime/vim-wakatime",
+		lazy = false
+	},
 	-- colourscheme
 	"bluz71/vim-moonfly-colors",
 })
+-- trouble
+require("trouble").setup()
 -- treesitter
 require("nvim-treesitter.configs").setup({
 	ensure_installed = {"c", "lua", "vim", "vimdoc", "query"},
@@ -88,21 +98,25 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 -- telescope
-require('telescope').setup({
+require("telescope").setup({
 	defaults = {
 		layout_config = {
 			bottom_pane = {
 				height = 100,
-				preview_cutoff = 120,
+				preview_cutoff = 100,
 				prompt_position = "bottom"
 			},
 		},
-		layout_strategy = "bottom_pane"
+		layout_strategy = "bottom_pane",
+		border = false
 	}
 })
-require('telescope').load_extension('dap')
+require("telescope").load_extension("dap")
+require("telescope").load_extension("aerial")
+require("telescope").load_extension("telescope-tabs")
+require("telescope-tabs").setup()
 -- lsp zero
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 lsp_zero.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -132,6 +146,7 @@ require("mason-lspconfig").setup({
 		end,
 	}
 })
+
 -- cmp
 local cmp = require("cmp")
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
@@ -145,14 +160,54 @@ cmp.setup({
 	},
 	formatting = lsp_zero.cmp_format(),
 	mapping = cmp.mapping.preset.insert({
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
 		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-		["<C-j>"] = cmp.mapping.confirm({ select = true }),
-		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+		["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+		["<C-l>"] = cmp.mapping.confirm({ select = true }),
+		["<C-;>"] = cmp.mapping.confirm({ select = true }),
+		["<C-Enter>"] = cmp.mapping.complete(),
 	}),
 })
 -- dap
 local dap = require("dap")
+dap.adapters.gdb = {
+	type = "executable",
+	command = "gdb",
+	args = { "-i", "dap" }
+}
+dap.adapters.codelldb = {
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = "/home/adam/src/codelldb/extension/adapter/codelldb",
+		args = {"--port", "${port}"},
+	}
+}
+dap.configurations.c = {
+	{
+		name = "GDB Native",
+		type = "gdb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Target: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtBeginningOfMainSubprogram = false,
+	},
+	{
+		name = "CodeLLDB",
+		type = "codelldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Target: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+	},
+}
 require("dapui").setup({
 	controls = {
 		element = "repl",
@@ -187,20 +242,16 @@ require("dapui").setup({
 		{
 			elements = {
 				{
-					id = "console",
-					size = 0.2
-				},
-				{
 					id = "watches",
-					size = 0.8
+					size = 0.6
 				},
 				{
 					id = "scopes",
-					size = 0.3
+					size = 0.4
 				},
 			},
-			position = "left",
-			size = 50
+			position = "right",
+			size = 80
 		},
 	},
 	mappings = {
@@ -216,113 +267,57 @@ require("dapui").setup({
 		max_value_lines = 100
 	}
 })
-local mason_dap = require("mason-nvim-dap")
-mason_dap.setup({
-	handlers = {
-		function(config)
-			mason_dap.default_setup(config)
-		end
-	}
-})
-require("nvim-dap-virtual-text").setup({
-	commented = true,
-})
--- gitsigns
-require('gitsigns').setup {
-	signs = {
-		add          = { text = '+' },
-		change       = { text = '~' },
-		delete       = { text = '-' },
-		topdelete    = { text = '-' },
-		changedelete = { text = '~' },
-		untracked    = { text = '/' },
-	},
-	signcolumn = true,
-	numhl      = false,
-	linehl     = false,
-	word_diff  = false,
-	watch_gitdir = {
-		follow_files = true
-	},
-	auto_attach = true,
-	attach_to_untracked = false,
-	current_line_blame = false,
-	current_line_blame_opts = {
-		virt_text = true,
-		virt_text_pos = 'eol',
-		delay = 1000,
-		ignore_whitespace = false,
-		virt_text_priority = 100,
-	},
-	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-	sign_priority = 6,
-	update_debounce = 100,
-	status_formatter = nil,
-	max_file_length = 40000,
-	preview_config = {
-		border = 'single',
-		style = 'minimal',
-		relative = 'cursor',
-		row = 0,
-		col = 1
-	},
-	yadm = {
-		enable = false
-	},
-}
+-- git
+require("gitsigns").setup()
 -- aerial
 local aerial_size = 35
 require("aerial").setup({
-	backends = { "treesitter", "lsp", "markdown", "man" },
+	backends = {"treesitter", "lsp", "markdown", "man"},
 	layout = {
 		max_width = aerial_size,
 		width = aerial_size,
 		min_width = aerial_size,
-		default_direction = "prefer_left",
-		placement = "edge",
+		default_direction = "left",
+		placement = "window",
 		resize_to_content = false,
 		preserve_equality = false,
 	},
 })
--- overseer
-require("overseer").setup({
-	templates = {"builtin", "user.run_script"},
-})
--- tool window
-local g_tool_flip = 0
-function Tool_Flip()
-	if g_tool_flip == 0 then
-		vim.cmd[[AerialOpen!]]
-		require("dapui").close()
-		g_tool_flip = 1
-	else
-		vim.cmd[[AerialClose]]
-		require("dapui").open()
-		g_tool_flip = 0
-	end
-end
+-- hex
+require("hex").setup()
+-- web devicons
+require("nvim-web-devicons").setup()
 -- keymaps
+vim.keymap.set("n", "`", "<nop>")
+vim.keymap.set("v", "`", "<nop>")
+vim.keymap.set("t", "`", "<nop>")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>a", "ggVG")
-vim.keymap.set("n", "<leader>x", function() vim.cmd[[b#|bd#]] end)
-vim.keymap.set("n", "<leader>ww", function() Tool_Flip() end)
-vim.keymap.set("n", "<leader>wa", function() vim.cmd[[AerialToggle!]] end)
-vim.keymap.set("n", "<leader>ee", function() vim.cmd[[Ex]] end)
 vim.keymap.set("n", "<leader>qq", function() require("telescope.builtin").diagnostics() end)
-vim.keymap.set("n", "<leader>fe", function() require("telescope.builtin").find_files() end)
+vim.keymap.set("n", "<leader>qw", function() require("trouble").toggle() end)
 vim.keymap.set("n", "<leader>ss", function() require("telescope.builtin").current_buffer_fuzzy_find() end)
-vim.keymap.set("n", "<leader>sl", function() require("telescope.builtin").live_grep() end)
-vim.keymap.set("n", "<leader>ff", function() require("telescope.builtin").treesitter() end)
+vim.keymap.set("n", "<leader>s_", function() require("telescope.builtin").live_grep() end)
+vim.keymap.set("n", "<leader>sl", function() require("telescope.builtin").grep_string({search = vim.fn.input("Grep: ")}) end)
+vim.keymap.set("n", "<leader>ff", function() require("telescope.builtin").lsp_document_symbols() end)
+vim.keymap.set("n", "<leader>fk", function() require("telescope.builtin").lsp_workspace_symbols() end)
+vim.keymap.set("n", "<leader>fa", function() require("telescope").extensions.aerial.aerial() end)
+vim.keymap.set("n", "<leader>fo", function() require("telescope.builtin").find_files() end)
 vim.keymap.set("n", "<leader>fh", function() require("telescope.builtin").help_tags() end)
 vim.keymap.set("n", "<leader>fc", function() require("telescope.builtin").commands() end)
 vim.keymap.set("n", "<leader>fj", function() require("telescope.builtin").jumplist() end)
 vim.keymap.set("n", "<leader>fr", function() require("telescope.builtin").registers() end)
+vim.keymap.set("n", "<leader>ft", function() require("telescope.builtin").treesitter() end)
 vim.keymap.set("n", "<leader>bb", function() require("telescope.builtin").buffers() end)
-vim.keymap.set("n", "<leader>dc", function() require("telescope").extensions.dap.commands() end)
-vim.keymap.set("n", "<leader>dq", function() require("telescope").extensions.dap.configurations() end)
-vim.keymap.set("n", "<leader>db", function() require("telescope").extensions.dap.list_breakpoints() end)
---vim.keymap.set("n", "<leader>dv", function() require("telescope").extensions.dap.variables() end)
+vim.keymap.set("n", "<leader>bn", function() vim.cmd[[enew]] end)
+vim.keymap.set("n", "<leader>bx", function() vim.cmd[[b#|bd#]] end)
+vim.keymap.set("n", "<leader>tt", function() require("telescope-tabs").list_tabs() end)
+vim.keymap.set("n", "<leader>tn", function() vim.cmd[[tab split]] end)
+vim.keymap.set("n", "<leader>tx", function() vim.cmd[[tabclose]] end)
+vim.keymap.set("n", "<leader>dv", function() require("telescope").extensions.dap.variables() end)
 vim.keymap.set("n", "<leader>df", function() require("telescope").extensions.dap.frames() end)
+vim.keymap.set("n", "<leader>dc", function() require("telescope").extensions.dap.commands() end)
+vim.keymap.set("n", "<leader>db", function() require("telescope").extensions.dap.list_breakpoints() end)
+vim.keymap.set("n", "<leader>dq", function() require("telescope").extensions.dap.configurations() end)
 vim.keymap.set("n", "<leader>dr", function() require("dap").repl.toggle() end)
 vim.keymap.set("n", "<leader>dl", function() require("dap").run_last() end)
 vim.keymap.set("n", "<leader>dd", function() require("dap").continue() end)
@@ -335,16 +330,19 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-Up>", function() vim.cmd[[resize -2]] end)
-vim.keymap.set("n", "<C-Down>", function() vim.cmd[[resize +2]] end)
-vim.keymap.set("n", "<C-Right>", function() vim.cmd[[vertical resize -2]] end)
-vim.keymap.set("n", "<C-Left>", function() vim.cmd[[vertical resize +2]] end)
+vim.keymap.set("n", "<C-n>", function() vim.cmd[[AerialNext]] end)
+vim.keymap.set("n", "<C-p>", function() vim.cmd[[AerialPrev]] end)
+vim.keymap.set("n", "<C-Up>", function() vim.cmd[[resize -1]] end)
+vim.keymap.set("n", "<C-Down>", function() vim.cmd[[resize +1]] end)
+vim.keymap.set("n", "<C-Right>", function() vim.cmd[[vertical resize -1]] end)
+vim.keymap.set("n", "<C-Left>", function() vim.cmd[[vertical resize +1]] end)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- colourscheme
 vim.cmd [[colorscheme moonfly]]
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
---vim.api.nvim_set_hl(0, "FloatBorder", {fg = "none", bg = "none"})
-vim.api.nvim_set_hl(0, "EndOfBuffer", {bg = "none"})
+vim.api.nvim_set_hl(0, "Normal", {fg = "none", bg = "none"})
+vim.api.nvim_set_hl(0, "NormalNC", {fg = "none", bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {fg = "none", bg = "none"})
+vim.api.nvim_set_hl(0, "NormalBorder", {fg = "none", bg = "none"})
+vim.api.nvim_set_hl(0, "FloatBorder", {fg = "none", bg = "none"})
+vim.api.nvim_set_hl(0, "EndOfBuffer", {fg = "none", bg = "none"})
---
+++ update.sh
@@ -0,0 +1,3 @@
+cp /home/adam/.config/tmux/tmux.conf ./
+cp /home/adam/.config/nvim/init.lua ./
+cp /home/adam/.gdbinit ./
